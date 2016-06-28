49c7a81341e6f196e775fcfed3a2a97808b9a2a0
jjram19 <jjram@stanford.edu>
2016-06-28 13:07:43 -0500

    Added SGD and testing

--- nngraphprac.lua
+++ nngraphprac.lua
@@ -7,61 +7,65 @@ require 'optim'
 --DOWNLOAD MNIST
 mnist = nnutils.MNISTLoader()
 inputs, labels = mnist:getTrainset()
-print('reached')
 
 mean = { }
 stdv = { }
-mean[1] = inputs[{ {}, {1}, {}, {}  }]:mean() -- mean estimation
+mean[1] = inputs[{ {}, {1}, {}, {}  }]:mean() 
 print('Channel ' .. 1 .. ', Mean: ' .. mean[1])
 inputs[{ {}, {1}, {}, {}  }]:add(-mean[1]) -- mean subtraction
             
-stdv[1] = inputs[{ {}, {1}, {}, {}  }]:std() -- std estimation
+stdv[1] = inputs[{ {}, {1}, {}, {}  }]:std() 
 print('Channel ' .. 1 .. ', Standard Deviation: ' .. stdv[1])
 inputs[{ {}, {1}, {}, {}  }]:div(stdv[1]) -- std scaling
 
 net = nn.Sequential()
-net:add(nn.SpatialConvolution(1, 6, 5, 5))
+net:add(nn.SpatialConvolution(1, 6, 5, 5)) -- 1 input image channel, 6 output channels, 5x5 convolution kernel
 net:add(nn.ReLU())                        
-net:add(nn.SpatialMaxPooling(2,2,2,2))    
+net:add(nn.SpatialMaxPooling(2,2,2,2))     
 net:add(nn.SpatialConvolution(6, 16, 5, 5))
-net:add(nn.ReLU())                       
+net:add(nn.ReLU())                        
 net:add(nn.SpatialMaxPooling(2,2,2,2))
-net:add(nn.View(16*5*5))                    
-net:add(nn.Linear(16*5*5, 120))            
-net:add(nn.ReLU())                     
+net:add(nn.View(16*5*5))                    -- reshapes from a 3D tensor to 1D (16x5x5 --> 16*5*5)
+net:add(nn.Linear(16*5*5, 120))             -- First layer of network
+net:add(nn.ReLU())                       
 net:add(nn.Linear(120, 84))
 net:add(nn.ReLU())                      
-net:add(nn.Linear(84, 10))                 
-net:add(nn.LogSoftMax())      
+net:add(nn.Linear(84, 10))                   -- 10 is the number of outputs of the network 
+net:add(nn.LogSoftMax())       -- converts the output to a log-probability. Useful for classification problems
 
 
 local criterion = nn.ClassNLLCriterion()
 local params, gradParams = net:getParameters()
 local optimState = {learningRate=0.01}
 
-for epoch=1,1 do
-    local function feval(params)
-        gradParams:zero()      
-        local outputs = net:forward(inputs)
-        local loss = criterion:forward(outputs, labels)
-        local dloss_doutput = criterion:backward(outputs, labels)
-        net:backward(inputs, dloss_doutput)
-        return loss,gradParams
+local batchSize = 128
+mnist:startEpoch(batchSize)
+
+for epoch = 1, 10 do
+    for x = 1, mnist:nBatches() do
+        inputsX, labelsX = mnist:getMiniBatch(x)
+        local function feval(params)
+            gradParams:zero()      
+            local outputs = net:forward(inputsX)
+            local loss = criterion:forward(outputs, labelsX)
+            local dloss_doutput = criterion:backward(outputs, labelsX)
+            net:backward(inputsX, dloss_doutput)
+            return loss,gradParams
+        end
+        optim.sgd(feval, params, optimState)
     end
-    print('-------')
-    print(epoch)
-    optim.sgd(feval, params, optimState)
 end
-print('boutta test')
+
+test_inputs, test_labels = mnist:getTestset()
 correct = 0
-for i=1,10000 do
-    local groundtruth = labels[i]
-    local prediction = net:forward(inputs[i])
-    local confidences, indices = torch.sort(prediction, true)  
+for i=1, 10000 do
+    local groundtruth = test_labels[i]
+    local prediction = net:forward(test_inputs[i])
+    local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
     if groundtruth == indices[1] then
         correct = correct + 1
     end
 end
 
-print(correct, 100*correct/10000 .. ' % ')
+print('The accuracy when compared with MNIST test data is ' .. 100*correct/10000 .. ' % ')
 
