282cd1cec84b95128f76907b0a97dfd6c6138b0c
jjram19 <jjram@stanford.edu>
2016-06-27 23:18:02 -0500

    Updated

--- nngraphprac.lua
+++ nngraphprac.lua
@@ -20,19 +20,19 @@ print('Channel ' .. 1 .. ', Standard Deviation: ' .. stdv[1])
 inputs[{ {}, {1}, {}, {}  }]:div(stdv[1]) -- std scaling
 
 net = nn.Sequential()
-net:add(nn.SpatialConvolution(1, 6, 5, 5)) -- 3 input image channels, 6 output channels, 5x5 convolution kernel
-net:add(nn.ReLU())                       -- non-linearity 
-net:add(nn.SpatialMaxPooling(2,2,2,2))     -- A max-pooling operation that looks at 2x2 windows and finds the max.
+net:add(nn.SpatialConvolution(1, 6, 5, 5))
+net:add(nn.ReLU())                        
+net:add(nn.SpatialMaxPooling(2,2,2,2))    
 net:add(nn.SpatialConvolution(6, 16, 5, 5))
-net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.ReLU())                       
 net:add(nn.SpatialMaxPooling(2,2,2,2))
-net:add(nn.View(16*5*5))                    -- reshapes from a 3D tensor of 16x5x5 into 1D tensor of 16*5*5
-net:add(nn.Linear(16*5*5, 120))             -- fully connected layer (matrix multiplication between input and weights)
-net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.View(16*5*5))                    
+net:add(nn.Linear(16*5*5, 120))            
+net:add(nn.ReLU())                     
 net:add(nn.Linear(120, 84))
-net:add(nn.ReLU())                       -- non-linearity 
-net:add(nn.Linear(84, 10))                   -- 10 is the number of outputs of the network (in this case, 10 digits)
-net:add(nn.LogSoftMax())       -- converts the output to a log-probability. Useful for classification problems
+net:add(nn.ReLU())                      
+net:add(nn.Linear(84, 10))                 
+net:add(nn.LogSoftMax())      
 
 
 local criterion = nn.ClassNLLCriterion()
@@ -55,10 +55,9 @@ end
 print('boutta test')
 correct = 0
 for i=1,10000 do
-    print(i)
     local groundtruth = labels[i]
     local prediction = net:forward(inputs[i])
-    local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
+    local confidences, indices = torch.sort(prediction, true)  
     if groundtruth == indices[1] then
         correct = correct + 1
     end
